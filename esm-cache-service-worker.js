c2a24a077ad675a8e677f44a7ed6be29e75a0b39
redaphid <iam@aaronherres.com>
2024-08-01 00:21:56 -0700

    only return cache response if the response was ok

--- esm-cache-service-worker.js
+++ esm-cache-service-worker.js
@@ -8,7 +8,7 @@ const cacheCdnRequests = async (request) =>{
 
   const cache = await caches.open(CACHE_NAME);
   const response = await cache.match(request);
-  if(response) return response
+  if(response?.ok) return response
 
   const netResponse = await fetch(request);
   cache.put(request, netResponse.clone());
