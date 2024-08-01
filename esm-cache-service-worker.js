6ea22ec57a910cf7299087dedc00f1b9777eb4b7
redaphid <iam@aaronherres.com>
2024-08-01 00:20:37 -0700

    more signals experiments. Auto esm.sh cdn caching

--- esm-cache-service-worker.js
+++ esm-cache-service-worker.js
@@ -1,34 +1,20 @@
 const CACHE_NAME = 'esm-cache-v1';
 
-self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
-  console.log('Service Worker installing...');
-});
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-  console.log('Service Worker activated!');
-});
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  
-  if (url.origin === 'https://esm.sh') {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(event.request).then(response => {
-          if (response) {
-            return response;
-          }
-          
-          return fetch(event.request).then(networkResponse => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        });
-      })
-    );
-  } else {
-    event.respondWith(fetch(event.request));
-  }
+const cacheCdnRequests = async (request) =>{
+  const url = new URL(request.url);
+  if (url.origin !== 'https://esm.sh') return fetch(request);
+
+  const cache = await caches.open(CACHE_NAME);
+  const response = await cache.match(request);
+  if(response) return response
+
+  const netResponse = await fetch(request);
+  cache.put(request, netResponse.clone());
+  return netResponse;
+}
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(cacheCdnRequests(event.request))
 });
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
   import { render } from "preact";
   import { html } from 'htm';
-  import { signal } from 'preact/signals';
+  import { signal, computed } from 'preact/signals';
 
-  const count = signal(0);
-  // get
+  const name = signal('Aaron');
+  const charactersInName = computed(()=>name.value.length);
+  const isAHacker = computed(()=>name.value === 'redaphid');
   const App = () => html`
     <div>
-      <h1 class>Hello World!</h1>
-      <button onClick=${() => (count.value += 1)}>
-        Increment with signal
-      </button>
-      <p>Counter: ${count}</p>
-    </div>
+    <h1 class=${isAHacker.value === true ? 'hacker': 'not-a-hacker'}>Hi ${name}</h1>
+    <h2>You have ${charactersInName} characters in your name</h2>
+    <form>
+      <label>a <input value=${name} onInput=${(e)=>name.value = e.currentTarget.value} /></label>
+    </form>
+  </div>
   `;
 
   render(html`<${App} />`, document.body);
