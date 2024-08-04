7b5bd0ef31eb33d46ed69a844545a2257960f899
redaphid <iam@aaronherres/com>
2024-08-04 19:28:18 -0400

    trying out signals interaction

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
 import { html } from 'htm';
 import { signal, computed, effect } from 'preact/signals';
 import {OpenAI} from 'openai'
-const openapiKey = signal(localStorage.getItem("OPENAPI_KEY") ?? '');
 
-const configOpen = signal(true);
+const openApiKey = signal(localStorage.getItem("OPENAPI_KEY") ?? '');
+const openApiKeyValid = signal(localStorage.getItem("OPENAPI_KEY_VALID") === "true");
+
+const configOpen = signal(!openApiKeyValid.value);
+const openApiTestResult = signal(openApiKeyValid.value ? "SUCCESSFULL": "NOT_TESTED");
 
 effect(()=>{
-  localStorage.setItem("OPENAPI_KEY", openapiKey.value);
+  localStorage.setItem("OPENAPI_KEY", openApiKey.value);
+  localStorage.setItem("OPENAPI_KEY_VALID", String(openApiTestResult.value === 'SUCCESSFULL'));
+  if(openApiTestResult.value === "FAILED") configOpen.value = true;
 })
 
-const openApiTestResult = signal("NOT_TESTED");
+effect(()=>{
+  openApiKeyValid.value = (openApiTestResult.value === 'OPENAPI_KEY_VALID');
+})
 
-const testOpenApiKey = async () => {
-  console.log("Testing OpenAPI Key");
-  const openai = new OpenAI({apiKey: openapiKey.value, dangerouslyAllowBrowser: true});
+const testopenApiKey = async () => {
+  const openai = new OpenAI({apiKey: openApiKey.value, dangerouslyAllowBrowser: true});
+  openApiTestResult.value = "NOT_TESTED"
   try {
   await openai.chat.completions.create({
     messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o-mini',
   });
-  openApiTestResult.value = "SUCCESSFUL";
+  openApiTestResult.value = "SUCCESSFULL";
   } catch (e) {
     openApiTestResult.value = "FAILED";
   }
-
 }
 
 const OpenApiSuccessResult = () => {
   if (openApiTestResult.value === "NOT_TESTED") return "❔"
-  if (openApiTestResult.value === "SUCCESSFUL") return "✅"
+  if (openApiTestResult.value === "SUCCESSFULL") return "✅"
   if (openApiTestResult.value === "FAILED") return "❌"
   return "❓"
 }
@@ -36,8 +42,8 @@ const OpenApiSuccessResult = () => {
 export const App = () => html`
 <div>
   <${CollapsableSection} title="Configuration" open=${configOpen}>
-    <label> OpenAPI Key: <input value=${openapiKey} onChange=${(e)=>openapiKey.value = e.currentTarget.value}/> </label>
-    <button onClick=${testOpenApiKey}>Test</button>
+    <label> OpenAPI Key: <input value=${openApiKey} onChange=${(e)=>openApiKey.value = e.currentTarget.value}/> </label>
+    <button onClick=${testopenApiKey}>Test</button>
     <${OpenApiSuccessResult}/>
   </${CollapsableSection}>
 
